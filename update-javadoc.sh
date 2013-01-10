0acdd3a4af80951bdd1dfa7681348f58cdd2c2a0
James Dennes <jdennes@gmail.com>
2013-01-10 10:01:09 +0100

    Added update-javadoc.sh to automatically generate and publish documentation for the current version of the library.

---
+++ update-javadoc.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+## Generates and updates javadoc for createsend-java, hosted at:
+## http://campaignmonitor.github.com/createsend-java/doc/
+
+BUILD_DOC=build/doc
+BUILD_PAGES=build/gh-pages
+
+set -e
+
+rm -rf $BUILD_DOC $BUILD_PAGES
+git clone git@github.com:campaignmonitor/createsend-java.git $BUILD_PAGES -b gh-pages
+gradle doc
+VERSION=$(basename $BUILD_DOC/*)
+rsync -f 'exclude .git' -r --delete $BUILD_DOC/$VERSION/ $BUILD_PAGES/doc
+cd $BUILD_PAGES
+git add doc
+git commit -m "Generated javadoc for $VERSION" --allow-empty
+#git push origin gh-pages
+cd ../..
+rm -rf $BUILD_DOC $BUILD_PAGES

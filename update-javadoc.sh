33c8865ce0f774cda1411c48f17d28edd6b9fc45
James Dennes <jdennes@gmail.com>
2013-01-10 10:01:54 +0100

    Ensure documentation is published to the gh-pages branch.

--- update-javadoc.sh
+++ update-javadoc.sh
@@ -16,6 +16,6 @@ rsync -f 'exclude .git' -r --delete $BUILD_DOC/$VERSION/ $BUILD_PAGES/doc
 cd $BUILD_PAGES
 git add doc
 git commit -m "Generated javadoc for $VERSION" --allow-empty
-#git push origin gh-pages
+git push origin gh-pages
 cd ../..
 rm -rf $BUILD_DOC $BUILD_PAGES
